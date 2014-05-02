c53acbdad95a5841f38bf8b1e02b38fe18431b85
liupeng <dp90219@gmail.com>
2014-05-02 15:46:45 +0800

    Implement Instance Methods

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var Class = function(args) {
   this.constructor = args.initialize || function() {};
+
+  for(var i in args) {
+    if (args[i].hasOwnProperty && typeof args[i] == 'function' && i != 'initialize') {
+      constructor.prototype[i] = args[i];
+    }
+  }
+
   return this.constructor;
 }
 
