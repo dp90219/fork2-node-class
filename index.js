ade4860d9d26a4a51c84e0c239cbfb4c08d38cb8
liupeng <dp90219@gmail.com>
2014-05-02 19:47:01 +0800

    change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Class = function(args) {
   this.constructor = args.initialize || function() {};
 
   for(var i in args) {
-    if (args[i].hasOwnProperty && typeof args[i] == 'function' && i != 'initialize') {
+    if (typeof args[i] == 'function' && i != 'initialize') {
       this.constructor.prototype[i] = args[i];
     }
   }
