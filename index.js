8cd20d89a745817779489d6d9d8f9305578a19f6
liupeng <dp90219@gmail.com>
2014-05-03 00:07:24 +0800

    Warmup4, I think this is the correct answer

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-var Class = function(child, Parent) {
-  // this.constructor = child.initialize || function() {};
-  // this.constructor = function(){};
+module.exports = function(child) {
+  var classFunc = child.initialize || function() {};
 
-  if (Parent)
-     // this.constructor.prototype.constructor.prototype = parent.prototype;
-     this.constructor.prototype = new Parent();
+  for (var key in child) 
+    if (key != "initialize")
+      classFunc.prototype[key] = child[key];
 
-  for (var key in child) {
-    if (key != 'initialize' && typeof child[key] == 'function')
-      this.constructor.prototype[key] = child[key];
-  }
-
-  return this.constructor;
+  return classFunc;
 }
-
-module.exports = Class;
-
-
