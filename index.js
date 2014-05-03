ab45e5b37758ebe60e73f6d71b8d5bf0cb5ca963
liupeng <dp90219@gmail.com>
2014-05-03 10:00:48 +0800

    Implement Super

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ module.exports = function(child, ParentFunc) {
 
   // ChildFunc.prototype.constructor.prototype = ParentFunc.prototype;
 
-  for (var key in ParentFunc.prototype) {
-    ChildFunc.prototype[key] = ParentFunc.prototype[key];
+  // for (var key in ParentFunc.prototype) {
+  //   ChildFunc.prototype[key] = ParentFunc.prototype[key];
+  // }
+
+  var parent = new ParentFunc();
+  for (var key in parent) {
+    ChildFunc.prototype[key] = parent[key];
   }
 
   ChildFunc.__super__ = ParentFunc;
@@ -14,6 +19,11 @@ module.exports = function(child, ParentFunc) {
   for (var key in child)
     if (key != "initialize" && child.hasOwnProperty(key))
       ChildFunc.prototype[key] = child[key];
+
+  ChildFunc.prototype.super = function(name) {
+    var result = (new ParentFunc())[name].apply(this, [].slice.call(arguments, 1));
+    return result;
+  };
 
 
   return ChildFunc;
